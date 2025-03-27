9a28c0ad98e470bda6ad5d9f9122b885c4542084
MathiasCLB <coulomb.mathias@gmail.com>
2025-03-27 09:44:32 +0100

    serveur.py

--- projet/serveur.py
+++ projet/serveur.py
@@ -8,34 +8,55 @@ class Serveur_Perroquet_Threaded:
     def __init__(self):
         self.pub, self.priv = rsa.newkeys(1024)
         self.host = "localhost"
-        self.port = 8000
-    
+        
 
 
     def serve(self, client):
-        e = str(self.pub.e).encode('utf-8')
-        n = str(self.pub.n).encode('utf-8')
-        client.sendall(e)
-        client.sendall(n)
-
+        def echange(client):
+            e = str(self.pub.e).encode('utf-8')
+            n = str(self.pub.n).encode('utf-8')
+            client.sendall(e)
+            print("e envoyé")
+            client.sendall(n)
+            print("n envoyé")
+            clef_codee = client.recv(2048)
+            clef = rsa.decrypt(clef_codee, self.priv)
+            return clef     
+        clef = echange(client)
+        print(clef)
+        self.envoi('Bonjour du serveur', client, clef)
+        while True:
+            message = self.reception(client, clef)
+            print(message)
+            self.envoi(message, client, clef)
+            
         
     def serve_forever(self):
         sock = socket()
         host = "10.69.225.145"
-        port = 1996 
+        port = 2013
         sock.bind((host, port))
         sock.listen()
         while True:
             client, adresse = sock.accept()
             print(f"connecté à {adresse[0]} sur le port {adresse[1]}")
-            self.serve(client)
+            Thread(target=self.serve, args=[client]).start()
 
 
-def envoi(mess, client, clef):
-    pass
+    def envoi(self, mess, client, clef):
+        aes = pyaes.AESModeOfOperationCTR(clef)
+        code = aes.encrypt(mess.encode('utf-8'))
+        client.sendall(code)
+
+    def reception(self, client, clef):
+        aes = pyaes.AESModeOfOperationCTR(clef)
+        code = client.recv(2048)
+        message = aes.decrypt(code)
+        print(message)
+        return message.decode('utf-8')
+
+
+
 
 serveur = Serveur_Perroquet_Threaded()
-print(serveur.pub)
-print(serveur.pub.e)
-print(serveur.pub.n)
 serveur.serve_forever()
