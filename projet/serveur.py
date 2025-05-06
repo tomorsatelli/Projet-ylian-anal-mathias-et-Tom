8eeb61ed41fabf78738c3f29b3798ae198db8c64
MathiasCLB <coulomb.mathias@gmail.com>
2025-05-06 08:45:23 +0200

    serveur.py

--- projet/serveur.py
+++ projet/serveur.py
@@ -3,6 +3,9 @@ import pyaes
 from socket import socket
 from threading import Thread
 import random
+from mysql.connector import connect
+from hashlib import sha256
+import base64
 
 class Serveur_Perroquet_Threaded:
     def __init__(self):
@@ -17,18 +20,52 @@ class Serveur_Perroquet_Threaded:
             n = str(self.pub.n).encode('utf-8')
             client.sendall(e)
             print("e envoyé")
+            print(client.recv(1024).decode('utf-8'))
             client.sendall(n)
             print("n envoyé")
             clef_codee = client.recv(2048)
+            print(clef_codee)
             clef = rsa.decrypt(clef_codee, self.priv)
-            return clef     
+            return clef
+
+        def connection(client):
+            cnx = connect(user='coulomb', password='coulomb', host='localhost', database='projet')
+            cursor = cnx.cursor()
+            self.envoi('Nouvel utilisateur?', client, clef)
+            reponse = self.reception(client, clef)
+            print(reponse)
+            if reponse == 'Oui': 
+                self.envoi("Entrer votre nom", client, clef)
+                nom = self.reception(client, clef)
+                print(nom)
+                self.envoi("Entrer votre prénom", client, clef)
+                prenom = self.reception(client, clef)
+                print(prenom)
+                self.envoi("Créer un mot de passe", client, clef)
+                mdp = self.reception(client, clef)
+                print(mdp)
+                hash1 = sha256(mdp.encode('utf-8'))
+                print(hash1)
+                print(hash1.digest())
+                sha = base64.standard_b64encode(hash1.digest()).decode('utf-8')
+                requete = f'INSERT INTO Utilisateurs (NOM, PRENOM, MOT_DE_PASSE) VALUES("{nom}", "{prenom}", {hash1.digest()})'
+                print(requete)
+                cursor.execute(requete)
+                cnx.commit()
+                resultat = cursor.fetchall()
+                print(resultat)
+                requete = "select * from Utilisateurs"
+                cursor.execute(requete)
+                resulat = cursor.fetchall()
+                cnx.commit()
+                print(resulat)
+
         clef = echange(client)
         print(clef)
         self.envoi('Bonjour du serveur', client, clef)
-        while True:
-            message = self.reception(client, clef)
-            print(message)
-            self.envoi(message, client, clef)
+        print(self.reception(client, clef))
+        connection(client)
+        
             
         
     def serve_forever(self):
@@ -52,7 +89,6 @@ class Serveur_Perroquet_Threaded:
         aes = pyaes.AESModeOfOperationCTR(clef)
         code = client.recv(2048)
         message = aes.decrypt(code)
-        print(message)
         return message.decode('utf-8')
 
 
